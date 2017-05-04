84debc14c6396bedb0d1ce88b5a974d900f3b3fc
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2017-05-04 14:22:32 +0200

    Add XMPP deps and rebrand to mim_iot_rpi0
    The additional dependency is expat.

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule NervesSystemRpi0.Mixfile do
+defmodule MimIotRpi0.Mixfile do
   use Mix.Project
 
   @version Path.join(__DIR__, "VERSION")
@@ -6,7 +6,7 @@ defmodule NervesSystemRpi0.Mixfile do
     |> String.strip
 
   def project do
-    [app: :nerves_system_rpi0,
+    [app: :mim_iot_rpi0,
      version: @version,
      elixir: "~> 1.3",
      compilers: Mix.compilers ++ [:nerves_package],
@@ -33,9 +33,9 @@ defmodule NervesSystemRpi0.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Timothy Mecklem", "Frank Hunleth"],
+    [maintainers: ["Szymon Mentel"],
     files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs", "README.md", "VERSION", "rootfs-additions", "fwup.conf", "cmdline.txt", "linux-4.4.defconfig", "config.txt", "post-createfs.sh"],
      licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/nerves-project/nerves_system_rpi0"}]
+     links: %{"Github" => "https://github.com/mentels/mim_iot_rpi0"}]
   end
 end
